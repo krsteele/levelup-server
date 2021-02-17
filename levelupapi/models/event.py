1ca09fcfe79b53cf4e0d5a719bca4fef841cc1de
Kristen Steele <ksteelephoto@gmail.com>
2021-02-17 11:30:21 -0600

    adds custom property to event model

--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -7,3 +7,11 @@ class Event(models.Model):
     game = models.ForeignKey("Game", on_delete=models.CASCADE)
     event_time = models.DateTimeField(auto_now=False, auto_now_add=False)
     location = models.CharField(max_length=50)
+
+    @property
+    def joined(self):
+        return self.__joined
+
+    @joined.setter
+    def joined(self, value):
+        self.__joined = value
